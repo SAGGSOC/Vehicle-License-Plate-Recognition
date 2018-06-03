70bc3da55c07e81a61ea3437bb635b8d9283d80c
Sagar Shivani <sagarshvn@gmail.com>
2018-06-04 00:39:30 +0530

    Preprocessing files

---
+++ Preprocess.py
@@ -0,0 +1,103 @@
+'''
+BSD 3-Clause License
+
+Copyright (c) 2018, Sagar Shivani
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistribution of this sofware without the permission of the owner will be 
+  dealt as copyright hinderence and is a punishable offence.
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+# Preprocess.py
+
+import cv2
+import numpy as np
+import math
+
+# module level variables ##########################################################################
+GAUSSIAN_SMOOTH_FILTER_SIZE = (5, 5)
+ADAPTIVE_THRESH_BLOCK_SIZE = 19
+ADAPTIVE_THRESH_WEIGHT = 9
+
+###################################################################################################
+def preprocess(imgOriginal):
+    imgGrayscale = extractValue(imgOriginal)
+
+    imgMaxContrastGrayscale = maximizeContrast(imgGrayscale)
+
+    height, width = imgGrayscale.shape
+
+    imgBlurred = np.zeros((height, width, 1), np.uint8)
+
+    imgBlurred = cv2.GaussianBlur(imgMaxContrastGrayscale, GAUSSIAN_SMOOTH_FILTER_SIZE, 0)
+
+    imgThresh = cv2.adaptiveThreshold(imgBlurred, 255.0, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, ADAPTIVE_THRESH_BLOCK_SIZE, ADAPTIVE_THRESH_WEIGHT)
+
+    return imgGrayscale, imgThresh
+# end function
+
+###################################################################################################
+def extractValue(imgOriginal):
+    height, width, numChannels = imgOriginal.shape
+
+    imgHSV = np.zeros((height, width, 3), np.uint8)
+
+    imgHSV = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
+
+    imgHue, imgSaturation, imgValue = cv2.split(imgHSV)
+
+    return imgValue
+# end function
+
+###################################################################################################
+def maximizeContrast(imgGrayscale):
+
+    height, width = imgGrayscale.shape
+
+    imgTopHat = np.zeros((height, width, 1), np.uint8)
+    imgBlackHat = np.zeros((height, width, 1), np.uint8)
+
+    structuringElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+    imgTopHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_TOPHAT, structuringElement)
+    imgBlackHat = cv2.morphologyEx(imgGrayscale, cv2.MORPH_BLACKHAT, structuringElement)
+
+    imgGrayscalePlusTopHat = cv2.add(imgGrayscale, imgTopHat)
+    imgGrayscalePlusTopHatMinusBlackHat = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+
+    return imgGrayscalePlusTopHatMinusBlackHat
+# end function
+
+
+
+
+
+
+
+
+
+
